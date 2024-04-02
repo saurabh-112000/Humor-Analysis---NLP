9caa3aff2e70e5a009a6916b762e4fff467785af
stuti04 <stutidhebar@gmail.com>
2024-04-01 20:31:33 -0400

    updated code

--- data_extraction.py
+++ data_extraction.py
@@ -21,7 +21,7 @@ def extract_hyperlinks_from_excel(excel_path, header_name='URL'):
     return urls
 
 
-excel_path = "data/transcript_urls.xlsx" 
+excel_path = "data/transcripts_urls.xlsx" 
 urls = extract_hyperlinks_from_excel(excel_path, header_name='URL')
 df_urls = pd.DataFrame(urls, columns=['URL'])
 print(df_urls)
